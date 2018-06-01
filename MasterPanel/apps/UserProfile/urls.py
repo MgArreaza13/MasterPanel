40d66d59072f5f2d86b07785211cbedaa708e815
Milennys <milennys.777@gmail.com>
2018-06-01 14:52:59 -0400

    Visualización de detalles del Programador

--- MasterPanel/apps/UserProfile/urls.py
+++ MasterPanel/apps/UserProfile/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from apps.UserProfile.views import ListaDeUsuarios
 from apps.UserProfile.views import ListaDeProgramadores
+from apps.UserProfile.views import DetallesDeProgramadores
 
 urlpatterns = [
 	url(r'^$', ListaDeUsuarios , name='ListaDeUsuarios'  ),
 	url(r'^Programadores/$', ListaDeProgramadores , name='ListaDeProgramadores'  ),
-
+	url(r'^Detalles/(?P<id_Programador>\d+)$', DetallesDeProgramadores , name='DetallesDeProgramadores'  ),
 ]
--- MasterPanel/apps/UserProfile/views.py
+++ MasterPanel/apps/UserProfile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 ##################MODELOS######################
 from apps.UserProfile.models import tb_profile
+from apps.Proyectos.models import tb_proyecto
+
 
 def ListaDeUsuarios(request):
 	ListaDeUsuarios = tb_profile.objects.all()
@@ -15,3 +17,15 @@ def ListaDeProgramadores(request):
 		'ListaDeProgramadores':ListaDeProgramadores
 	}
 	return render(request, 'UserProfile/listadeprogramadores.html', contexto)
+
+def DetallesDeProgramadores(request, id_Programador ):
+	DetallesDeProgramadores = tb_profile.objects.filter(tipoUser = "Programador")
+	Programador   = tb_profile.objects.get(id = id_Programador)
+	proyecto = tb_proyecto.objects.all()
+	contexto = {
+		'DetallesDeProgramadores':DetallesDeProgramadores,
+		'proyecto':proyecto,
+		'Programador':Programador
+
+	}
+	return render(request, 'UserProfile/DetallesDeProgramadores.html', contexto)
